09a75b80837d5dbbc666bfd108216338ec76220c
ttrebicki <33643514+ttrebicki@users.noreply.github.com>
2024-08-21 11:49:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -465,7 +465,7 @@ class Going {
         'transactionId',
         'extendedPayment',
         'token',
-        // 'error',
+        'error',
         'reservationId',
         'partnerId',
         'template',
@@ -553,7 +553,7 @@ class Going {
     return keys
       .map((key) => {
         if (this.state[key]) {
-          return `?${key}=${this.state[key]}`;
+          return `${key}=${this.state[key]}`;
         }
 
         return null;
