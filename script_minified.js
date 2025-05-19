0dfdd556f8b3a5dd56d12356e406f1f766863283
ttrebicki <33643514+ttrebicki@users.noreply.github.com>
2025-05-19 10:46:15 +0200

    Update script_minified.js

--- script_minified.js
+++ script_minified.js
@@ -751,7 +751,7 @@ class Going {
             window.top.document.documentElement.scrollTop;
 
       if (scrollPosY) {
-        this.setLastScrollPos(scrollPosY);
+        // this.setLastScrollPos(scrollPosY);
       }
 
       html.style.overflow = 'hidden';
